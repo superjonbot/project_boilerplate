dfd3c6b0290deac3be70193aea49e972628bf5de
Jonathan Robles <jonathanjarobles@gmail.com>
2017-04-19 02:32:34 -0400

    added proxy and ip checker

--- whatip.php
+++ whatip.php
@@ -3,12 +3,12 @@ header('Content-type: application/json');
 
 $return['ip'] = $_SERVER['REMOTE_ADDR'];
 $return['sip'] = $_SERVER['SERVER_ADDR'];
-$return['ipa'] = $_SERVER['HTTP_CLIENT_IP'];
-$return['ipb'] = $_SERVER['HTTP_X_FORWARDED_FOR'];
+//$return['ipa'] = $_SERVER['HTTP_CLIENT_IP'];
+//$return['ipb'] = $_SERVER['HTTP_X_FORWARDED_FOR'];
 $localIP = getHostByName(getHostName());
 
 $results = array(
-      'ip' => $return['ip'],'sip' => $return['sip'],'lip' => $localIP, 'ipa' => $return['ipa'],'ipb' => $return['ipb']
+      'ip' => $return['ip'],'sip' => $return['sip'],'lip' => $localIP/*, 'ipa' => $return['ipa'],'ipb' => $return['ipb']*/
 );
 echo json_encode($results);
 ?>
